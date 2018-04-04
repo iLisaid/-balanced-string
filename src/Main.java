3f28467729ba6a8d7c90c9ecc357f679ad0e678d
iLisaid <iLisaid@users.noreply.github.com>
2018-04-04 12:17:27 +0300

    all

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class main {
-    public static void Run(String[] args){
+public class Main {
+    public static void main(String[] args){
         System.out.print(StringBalanceChecker.stringBalanceCheck(args[0]));
     }
 }
